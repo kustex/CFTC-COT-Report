c2850c0b09927a3bbfbe97a25ff7066d3b06d6fe
Lucas Kustermans <44235638+kustex@users.noreply.github.com>
2024-10-07 11:29:40 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -86,7 +86,7 @@ which gunicorn
 # # Replace 'server:app' with 'yourfile:app' if your Flask instance is named differently.
 # # gunicorn --workers 3 --bind 0.0.0.0:8000 server:app &
 echo "starting gunicorn"
-sudo /var/www/cftc-app/venv/bin/gunicorn gunicorn -w 4 app_cftc:app
-# sudo gunicorn --workers 3 --bind unix:myapp.sock  app_cftc:app --user www-data --group www-data --daemon
+# sudo /var/www/cftc-app/venv/bin/gunicorn gunicorn -w 4 app_cftc:app
+sudo /var/www/cftc-app/venv/bin/gunicorn gunicorn --workers 3 --bind unix:myapp.sock  app_cftc:app --user www-data --group www-data --daemon
 
 echo "started gunicorn 🚀"
