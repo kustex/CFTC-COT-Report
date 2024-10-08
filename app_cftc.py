56dcfe251e3804e8e2e373bff5a3d67c11acf14d
Lucas Kustermans <lucaskustermans@outlook.com>
2024-10-08 13:27:55 +0200

    edited Dockerfile

--- app_cftc.py
+++ app_cftc.py
@@ -251,7 +251,5 @@ app.layout = html.Div([
 
 if __name__ == '__main__':
     app.run(
-        host=os.getenv('LISTEN', '0.0.0.0'),
-        port=int(os.getenv('PORT', '8080')),
-        debug=False
+        debug=True, host='0.0.0.0'
         )
